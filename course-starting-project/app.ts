6834f16617ddb0350188d58e22a4a1977a71f341
Paweł Drożański <pawdro@gmail.com>
2023-10-22 13:46:42 +0200

    Usage enums

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,16 +1,33 @@
 const birthYear = 1986;
 const currentYear = new Date().getFullYear();
 
-const person: {
-  name: string;
-  hobbies: string[];
-  age: number;
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   hobbies: string[];
+//   age: number;
+//   role: [number, string];
+// } = {
+//   name: "Paul",
+//   age: currentYear - birthYear,
+//   hobbies: ["Cooking", "Biking", "Photography"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
   name: "Paul",
   age: currentYear - birthYear,
   hobbies: ["Cooking", "Biking", "Photography"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role[1] = 10; // Cannot change elements in tuple
@@ -22,4 +39,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); // Error because hobby is interfered as string not array
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
