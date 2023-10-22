1eade20aaa62963e634ab73a727f1285a2070ec7
Paweł Drożański <pawdro@gmail.com>
2023-10-22 15:00:40 +0200

    Literal types are pretty cool

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,15 +1,29 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConverstion: "as-number" | "as-text", // pretty legit
+) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
+  if (resultConverstion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
+
   return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Anna");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
