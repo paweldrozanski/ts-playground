915c5cab6497e226e2c43c59110cbd97a8b44ef0
Paweł Drożański <pawdro@gmail.com>
2023-10-22 19:03:23 +0200

    Never return a value

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -6,3 +6,9 @@ userInput = "Paul";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+}
+
+generateError("An error occured", 500);
