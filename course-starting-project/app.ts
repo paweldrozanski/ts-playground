24ece4e6c43aa139e3ed4acae2fe6ce83312b765
Paweł Drożański <pawdro@gmail.com>
2023-10-22 17:32:42 +0200

    Return void or undefined

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,35 +1,10 @@
-type Combinable = number | string;
-type ConversionDescription = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConverstion: ConversionDescription,
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  if (resultConverstion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
-
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
-
-type User = { name: string; age: number };
-const u1: User = { name: "Paul", age: 37 };
+printResult(add(5, 12)); // returns void
+console.log(printResult(add(5, 12))); // returns undefined
