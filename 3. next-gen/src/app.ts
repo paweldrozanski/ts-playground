4b8ce65cc0d9791e4c15c8ddd6d229cdfc26c21d
Paweł Drożański <pawdro@gmail.com>
2023-10-25 16:43:19 +0200

    Scopes

--- 3. next-gen/src/app.ts
+++ 3. next-gen/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = "Paul";
+
+let age = 30;
+age = 37;
+
+let result;
+function add(a: number, b: number) {
+  result = a + b;
+  return result;
+}
+console.log(result);
+
+// not working due to block scoped 'age'
+// if (age > 30) {
+//   let isOld = true;
+// }
+// console.log(isOld);
