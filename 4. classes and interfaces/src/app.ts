14ce41e3a0e4d99bbb7711b55de96f5e871f709f
Paweł Drożański <pawdro@gmail.com>
2023-10-25 21:46:44 +0200

    Shorthand initialization

--- 4. classes and interfaces/src/app.ts
+++ 4. classes and interfaces/src/app.ts
@@ -1,13 +1,18 @@
 class Department {
-  private name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(
+    private id: string,
+    public name: string,
+  ) {
+    // this.id = id
+    // this.name = name;
   }
 
   describe(this: Department) {
-    console.log("Department:", this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +25,7 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 accounting.addEmployee("Ewelina");
 accounting.addEmployee("Adam");
 
