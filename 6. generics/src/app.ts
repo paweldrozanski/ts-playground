e82ce198fefb9a31343962582e1bacd69be62824
Paweł Drożański <pawdro@gmail.com>
2023-10-31 16:51:55 +0100

    Generic function

--- 6. generics/src/app.ts
+++ 6. generics/src/app.ts
@@ -1,12 +1,24 @@
-const names: Array<string> = ["Paul", "Ewelina"]; // string[]
+// const names: Array<string> = ["Paul", "Ewelina"]; // string[]
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-// won't pass because of Promise type (number)
-promise.then((data) => {
-  data.split(" ");
-});
+// // won't pass because of Promise type (number)
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+// Generics function
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+
+const mergedObj = merge(
+  { name: "Paul", hobbies: ["Cooking", "Fathering"] },
+  { age: 37 },
+);
+
+console.log(mergedObj);
