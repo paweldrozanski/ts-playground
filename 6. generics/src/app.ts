6506d378f7919ea2d5fe27acc2dd5b684e5c195f
Paweł Drożański <pawdro@gmail.com>
2023-10-31 16:33:24 +0100

    Promise + generics

--- 6. generics/src/app.ts
+++ 6. generics/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const names: Array<string> = ["Paul", "Ewelina"]; // string[]
+
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
+
+// won't pass because of Promise type (number)
+promise.then((data) => {
+  data.split(" ");
+});
