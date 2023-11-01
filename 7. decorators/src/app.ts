69ac8117945563847545b61c7928968f69fb6798
Paweł Drożański <pawdro@gmail.com>
2023-11-01 16:19:17 +0100

    Instead of 1 line use 28 lines :)

--- 7. decorators/src/app.ts
+++ 7. decorators/src/app.ts
@@ -101,3 +101,36 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function AutoBind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", p.showMessage);
