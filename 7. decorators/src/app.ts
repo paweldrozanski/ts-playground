5994a2efdff881e38ed7a7f2ad10bd1cc7e0dc3e
Paweł Drożański <pawdro@gmail.com>
2023-11-01 15:57:57 +0100

    Returning a class in class decorators lol, will use it for sure, hahah

--- 7. decorators/src/app.ts
+++ 7. decorators/src/app.ts
@@ -7,14 +7,21 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log("Rendering template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T,
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -32,7 +39,7 @@ class Person {
 
 const person = new Person();
 
-console.log(person);
+// console.log(person);
 
 // ------
 
