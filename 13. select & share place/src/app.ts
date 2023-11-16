6ecf4a9aae365c6094a3778468ba81198740c7b2
Paweł Drożański <pawdro@gmail.com>
2023-11-16 22:38:38 +0100

    Select and share final version

--- 13. select & share place/src/app.ts
+++ 13. select & share place/src/app.ts
@@ -1,5 +1,15 @@
+import axios from "axios";
+import { Loader } from "@googlemaps/js-api-loader";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "some-key";
+
+type GoogleDeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -7,6 +17,37 @@ function searchAddressHandler(event: Event) {
 
   console.log(enteredAddress);
   // send this to Google's API!
+  axios
+    .get<GoogleDeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+      )}&key=${GOOGLE_API_KEY}`,
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+
+      const loader = new Loader({
+        apiKey: GOOGLE_API_KEY,
+        version: "weekly",
+      });
+
+      loader.load().then(async () => {
+        const { Map } = (await google.maps.importLibrary(
+          "maps",
+        )) as google.maps.MapsLibrary;
+        new Map(document.getElementById("map") as HTMLElement, {
+          center: { lat: coordinates.lat, lng: coordinates.lng },
+          zoom: 16,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 form?.addEventListener("submit", searchAddressHandler);
