7c632604396a5ec5bfbb4bde36673e26146c4781
Paweł Drożański <pawdro@gmail.com>
2023-11-17 18:10:57 +0100

    Simple todo passed with props

--- reactwithts/src/App.tsx
+++ reactwithts/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const App: React.FunctionComponent = () => {
-  return <div className="App"></div>;
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
