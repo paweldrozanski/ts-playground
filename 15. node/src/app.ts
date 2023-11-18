ab99430aa189041581f40fd2fbb5e0d414a0e93e
Paweł Drożański <pawdro@gmail.com>
2023-11-18 20:08:07 +0100

    Create todo method

--- 15. node/src/app.ts
+++ 15. node/src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
+import { json } from "body-parser";
 import todoRoutes from "./routes/todos";
 
 const app = express();
 
+app.use(json());
+
 app.use("/todos", todoRoutes);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
