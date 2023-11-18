670c6cc035bb7784009e76bbfd4e3a8d9dd29546
Paweł Drożański <pawdro@gmail.com>
2023-11-18 14:18:52 +0100

    Delete todo functionality

--- reactwithts/src/App.tsx
+++ reactwithts/src/App.tsx
@@ -15,10 +15,16 @@ const App: React.FC = () => {
     ]);
   };
 
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAppHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
