d4a2b2471c3617b54ac925523a5c784ff398eb38
Paweł Drożański <pawdro@gmail.com>
2023-11-18 13:58:56 +0100

    Cross-component communication using props

--- reactwithts/src/App.tsx
+++ reactwithts/src/App.tsx
@@ -6,9 +6,13 @@ import NewTodo from "./components/NewTodo";
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
 
+  const todoAppHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAppHandler} />
       <TodoList items={todos} />
     </div>
   );
