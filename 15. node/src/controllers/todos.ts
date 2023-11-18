645a84a3f4b1f2a9284ba789ce4aafbade6b830b
Paweł Drożański <pawdro@gmail.com>
2023-11-18 20:34:24 +0100

    CRUD paths

--- 15. node/src/controllers/todos.ts
+++ 15. node/src/controllers/todos.ts
@@ -11,3 +11,36 @@ export const createTodo: RequestHandler = (req, res, _next) => {
 
   res.status(201).json({ message: "Created the todo.", createdToto: newTodo });
 };
+
+export const getTodos: RequestHandler = (_req, res, _next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Todo deleted!" });
+};
